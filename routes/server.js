9d068896883a49493eb23c93ff2543659a61fe4a
庞敬聪 <1043573731@qq.com>
2017-10-19 15:33:55 +0800

    auto upload

--- routes/server.js
+++ routes/server.js
@@ -37,6 +37,8 @@ var server = http.createServer(function (request, response) {
     });
 });
 
-server.listen(8080);
+server.listen(8080,function(){
+    console.log('start!')
+});
 
 console.log('Server is running at http://127.0.0.1:8080/');
